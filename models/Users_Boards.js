f0f735b4d0d5d11e2ae0464eb156faa2d3b63d8d
anrwn <andrenrwn@users.noreply.github.com>
2023-11-15 10:50:49 +0800

    Debugging users_boards

--- models/Users_Boards.js
+++ models/Users_Boards.js
@@ -1,11 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+const User = require('./User');
+const Boards = require('./Boards');
 
 class Users_Boards extends Model {}
 
 Users_Boards.init(
   {
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    board_board_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'boards',
+        key: 'board_id',
+      },
+    },
   },
   {
     sequelize,
