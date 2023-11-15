191b5c3b9e3b7975a54ebc1c21fa8a7ef5b326a4
anrwn <andrenrwn@users.noreply.github.com>
2023-11-15 09:48:49 +0800

    Added database model and seed data for tables Boards, Filters, Locations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,10 @@
 const User = require('./User');
-const Project = require('./Project');
+const Boards = require('./Boards');
+const Filters = require('./Filters');
+const Locations = require('./Locations');
+const Users_Boards = require('./Users_Boards');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+User.belongsToMany(Boards, { through: 'Users_Boards' });
+Boards.belongsToMany(User, { through: 'Users_Boards' });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-module.exports = { User, Project };
+module.exports = { User, Boards, Filters, Locations, Users_Boards };
